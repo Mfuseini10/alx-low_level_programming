dc2bd27de2199fe774cd2215659e3d27f3c3a04e
Mohammed Fuseini <mfuseini133@gmail.com>
2023-02-17 01:42:23 -0800

    upload 0 - 4 with README.md

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * Main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z' ; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}

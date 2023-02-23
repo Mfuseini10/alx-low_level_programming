dea1cfc6cd51eec725f2841899888898dc4a1a49
Mohammed Fuseini <mfuseini133@gmail.com>
2023-02-23 00:33:04 -0800

    first uploads

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,47 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * _putchar - writes the character c to std
+ * @c: parameter to compare
+ * Return: 1 or 0
+ */
+int _putchar(char c);
+/**
+ * Write a function that checks for uppercase character
+ *
+ */
+int _isupper(int c);
+/**
+ * Write a function that checks for a digit (0 through 9)
+ *
+ */
+int _isdigit(int c);
+/**
+ * Write a function that multiplies two integers.
+ *
+ */
+int mul(int a, int b);
+/**
+ * Write a function that prints the numbers, from 0 to 9, followed by a new line.
+ *
+ */
+void print_numbers(void);
+/**
+ * Write a function that prints the numbers, from 0 to 9, followed by a new line.
+ * Do not print 2 and 4
+ *
+ */
+void print_most_numbers(void);
+/**
+ * Write a function that prints 10 times the numbers, from 0 to 14, followed by a new line. 
+ *
+ */
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_numbers(void);
+
+#endif

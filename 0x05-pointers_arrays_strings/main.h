4c96166ef0472515050b9e570b3a32a202371f42
Mohammed Fuseini <mfuseini133@gmail.com>
2023-02-27 13:02:12 -0800

    0-reset_to_98.c and main.h

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,25 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+ * main - function that takes a pointer to an int as parameter and updates the value it points to to 98.
+ *
+ * Return: Always 0.
+ */
+void reset_to_98(int *n);
+/**
+ * main - function that swaps the values of two integers.
+ *
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+
+#endif

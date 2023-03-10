c579e998e9619ec30db8b1f72d3a27298852e583
Mohammed Fuseini <mfuseini133@gmail.com>
2023-03-10 13:14:48 -0800

    mynewnameis

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - main function
+ * @argc: argumentc
+ * @argv: vector of arguments
+ *Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("program name is: %s\n",argv[0]);
+
+	return 0;
+}

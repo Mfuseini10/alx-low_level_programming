b8d1485b2947cf85762cfb5f9671f079c5a915dc
Mohammed Fuseini <mfuseini133@gmail.com>
2023-09-28 17:36:15 +0000

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,21 +9,18 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int decimal_value = 0;
+	unsigned int deci_val = 0;
 
 	if (!b)
-		return (0); // Check if the input string is NULL
-	
-	// Iterate through the binary string
+		return (0);
+
 	for (i = 0; b[i]; i++)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-
-		// Calculate the decimal value using bitwise operations
-		decimal_value = 2 * decimal_value + (b[i] - '0');
+		deci_val = 2 * deci_val + (b[i] - '0');
 	}
 
-	return (decimal_value);
+	return (deci_val);
 }
 
